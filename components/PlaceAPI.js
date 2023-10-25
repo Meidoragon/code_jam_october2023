a9c1d04ba5eabe47e536eca07b45fa131e656675
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2023-10-25 11:22:08 -0500

    feat: add place api queries to get haunted houses

--- components/PlaceAPI.js
+++
@@ -1,6 +0,0 @@
-export default class PlaceAPI{
-
-    constructor(options){
-
-    }
-}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,44 @@
-import MapAPI from '../components/MapAPI.js'
-import { nycLATLNG as LOCATION } from '../utils/constants.js'
+import { nycLATLNG as LOCATION,  locationBias as BIAS} from '../utils/constants.js';
+
+const request = {
+  query: 'haunted house',
+  fields: ['name', 'formatted_address', 'price_level'],
+  locationBias: BIAS,
+}
 
 const mapElement = document.querySelector('.map');
-let map
+let map;
+let houses = [];
+let NYC;
+let service;
 const promises = [
   google.maps.importLibrary('maps'),
-  google.maps.importLibrary('geocoding')
+  google.maps.importLibrary('places'),
+  google.maps.importLibrary('marker'),
 ];
 
 function initGoogle() {
+
   return Promise.all(promises)
-    .then(([Maps, Geocoding]) => {
-      console.log(Maps);
+    .then(([Maps, Place, Markers]) => {
+      NYC = new google.maps.LatLng(LOCATION.lat, LOCATION.lng);
       map = new Maps.Map(mapElement, {
-        zoom: 9,
-        center: LOCATION,
-        mapId: "DEMO MAP ID"
+        zoom: 11,
+        center: NYC,
+        mapId: "Haunted House Map"
       })
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({location: NYC, radius: 20000, keyword: 'haunted house',}, placeCallback);
   })
+}
+
+function placeCallback(results, status){
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    console.log(results.length);
+    for (let i = 0; i < results.length; i++) {
+      console.log(results[i].name);
+    }
+  }
 }
 
 const googleAPI = initGoogle()
